383a5bde9a9889b4d951632827124b78dcb57b3b
Alex Weltman <alex@havenly.com>
2020-01-15 01:01:33 -0700

    got typeorm working

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,39 @@
 import bodyParser from 'body-parser';
 import express, { Request, Response } from 'express';
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
 
-const app = express();
-const port = process.env.PORT || 5000;
+createConnection().then(async (connection: any) => {
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    await connection.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
 
-app.get('/api/hello', (req: Request, res: Response) => {
-  res.send({ express: 'Hello From Express' });
-});
+    console.log("Loading users from the database...");
+    const users = await connection.manager.find(User);
+    console.log("Loaded users: ", users);
 
-app.get('/api/hello2', (req: Request, res: Response) => {
-  res.send({ express: 'Hello From Express 2' });
-});
+    const app = express();
+    const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.get('/api/hello', (req: Request, res: Response) => {
+      res.send({ express: 'Hello From Express' });
+    });
+
+    app.get('/api/users', async (req: Request, res: Response) => {
+      const users = await connection.manager.find(User);
+      res.send(users);
+    });
+
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+
+}).catch((error: any) => console.log(error));
+
+
